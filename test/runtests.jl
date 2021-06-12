ea4e4d182afdc7fe210ff428e9afe8687b9c117f
Annelle Abatoni Kayisire <aa1@wellesley.edu>
2021-06-12 16:09:01 +0200

    testing .tsv file

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using RCall
 reval("install.packages('vegan')")
 
 @testset "Biobakery Utilities" begin
-    abund = import_abundance_table("dev/BiobakeryUtils/test/metaphlan_test.tsv")
+    abund = import_abundance_table("metaphlan_test.tsv")
 
     @test typeof(abund) <: DataFrame
     @test size(abund) == (42, 8)
