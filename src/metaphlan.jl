1559162e9fc2e5b8d8319dc78492c8106401c5bf
Annelle Abatoni Kayisire <aa1@wellesley.edu>
2021-06-16 15:19:31 +0200

    Documentation of functions in metaphlan.jl

--- src/metaphlan.jl
+++ src/metaphlan.jl
@@ -41,6 +41,71 @@ Taxon level is removed from resulting taxon string, eg.
 `g__Bifidobacterium` becomes `Bifidobacterium`.
 
 Set `keepunidentified` flag to `false` to remove `UNIDENTIFIED` rows.
+
+`taxfilter!()` modifies the dataframe that you pass to it and `taxfilter()` doesn't.
+
+```jldoctest taxfilter! and taxfilter
+
+Examples
+≡≡≡≡≡≡≡≡≡≡
+julia> df
+4×2 DataFrame
+ Row │ taxon                          abundance 
+     │ String                         Float64   
+─────┼──────────────────────────────────────────
+   1 │ k__Bacteria                     100.0
+   2 │ k__Bacteria|p__Firmicutes        63.1582
+   3 │ k__Bacteria|p__Bacteroidetes     25.6038
+   4 │ k__Bacteria|p__Actinobacteria    11.0898
+
+julia> taxfilter(df,2; keepunidentified=true)
+4×2 DataFrame
+ Row │ taxon            abundance 
+     │ String           Float64   
+─────┼────────────────────────────
+   1 │ Firmicutes         63.1582
+   2 │ Bacteroidetes      25.6038
+   3 │ Actinobacteria     11.0898
+   4 │ Verrucomicrobia     0.1482
+
+julia> df
+4×2 DataFrame
+ Row │ taxon                          abundance 
+     │ String                         Float64   
+─────┼──────────────────────────────────────────
+   1 │ k__Bacteria                     100.0
+   2 │ k__Bacteria|p__Firmicutes        63.1582
+   3 │ k__Bacteria|p__Bacteroidetes     25.6038
+   4 │ k__Bacteria|p__Actinobacteria    11.0898
+
+julia> taxfilter!(df,2; keepunidentified=true)
+3×2 DataFrame
+ Row │ taxon           abundance 
+     │ String          Float64   
+─────┼───────────────────────────
+   1 │ Firmicutes        63.1582
+   2 │ Bacteroidetes     25.6038
+   3 │ Actinobacteria    11.0898
+
+julia> df
+3×2 DataFrame
+ Row │ taxon           abundance 
+     │ String          Float64   
+─────┼───────────────────────────
+   1 │ Firmicutes        63.1582
+   2 │ Bacteroidetes     25.6038
+   3 │ Actinobacteria    11.0898
+
+julia> taxfilter!(df,1; keepunidentified=true)
+3×2 DataFrame
+ Row │ taxon           abundance 
+     │ String          Float64   
+─────┼───────────────────────────
+   1 │ Firmicutes        63.1582
+   2 │ Bacteroidetes     25.6038
+   3 │ Actinobacteria    11.0898
+   
+```
 """
 function taxfilter!(taxonomic_profile::AbstractDataFrame, level::Int=7; keepunidentified=true)
     in(level, collect(1:8)) || @error "$level not a valid taxonomic level" taxonlevels
@@ -85,6 +150,19 @@ Levels may be given either as numbers or symbols:
 - `6` = `:Genus`
 - `7` = `:Species`
 - `8` = `:Subspecies`
+
+```jldoctest parsetaxon
+Examples
+≡≡≡≡≡≡≡≡≡≡
+
+ julia> parsetaxa("k__Archaea|p__Euryarchaeota|c__Methanobacteria";throw = true)
+ 3-element Vector{Tuple{String, Symbol}}:
+ ("Archaea", :kingdom)
+ ("Euryarchaeota", :phylum)
+ ("Methanobacteria", :class)
+
+```
+
 """
 function parsetaxon(taxstring::AbstractString, taxlevel::Int=7; throw=true)
     taxa = parsetaxa(taxstring, throw=throw)
