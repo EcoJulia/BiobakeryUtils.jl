14f009608d38f2a5c2b769ee55e1b15a32520290
Annelle Abatoni Kayisire <aa1@wellesley.edu>
2021-06-23 13:05:53 +0200

    updating runtests.jl with CommunityProfile tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,29 +7,28 @@ using BiobakeryUtils
 using SparseArrays
 using DelimitedFiles
 
-@testset "Data Import" begin
-    abund = import_abundance_table("test/metaphlan_test.tsv")
-    @test typeof(abund) <: DataFrame
-    @test size(abund) == (42, 8)
-    spec = taxfilter(abund, :kingdom, keepunidentified=true)
-    @test size(spec) == (15, 8)
-    phyl = taxfilter(abund, :phylum)
-    @test size(phyl) == (2, 8)
-    @test !any(occursin.("|", phyl[!, 1]))
-    taxfilter!(abund, 2)
-    @test abund == phyl
+# @testset "Data Import" begin
+#     abund = import_abundance_table("test/metaphlan_test.tsv")
+#     @test typeof(abund) <: DataFrame
+#     @test size(abund) == (42, 8)
+#     spec = taxfilter(abund, :kingdom, keepunidentified=true)
+#     @test size(spec) == (15, 8)
+#     phyl = taxfilter(abund, :phylum)
+#     @test size(phyl) == (2, 8)
+#     @test !any(occursin.("|", phyl[!, 1]))
+#     taxfilter!(abund, 2)
+#     @test abund == phyl
 
-end
+# end
 
 @testset "CommunityProfile Testing" begin
     table = CSV.read("test/metaphlan_test.tsv", DataFrame, delim='\t',
     header=["#SampleID", "sample1_taxonomic_profile", "sample2_taxonomic_profile", "sample3_taxonomic_profile",	"sample4_taxonomic_profile", "sample5_taxonomic_profile", "sample6_taxonomic_profile", "sample7_taxonomic_profile"], datarow = 8)
     rename!(table, "#SampleID" => "taxname")
     mat = Matrix(select(table, Not("taxname")))
-    mat = coalesce.(abund, 0.) #turns all missings to 0
-    tax = Taxon.(p1.taxname)
-    mss = MicrobiomeSample.(names(p1)[1:end])
-    cp = CommunityProfile(sparse(mat), taxa, samples) # sparse turns matrix into sparse matrix
+    tax = Taxon.(table.taxname)
+    mss = MicrobiomeSample.(names(table)[2:end])
+    cp = CommunityProfile(sparse(mat), tax, mss) # sparse turns matrix into sparse matrix
     @test typeof(cp) <: CommunityProfile
     @test size(cp) == (36,7)
     @test cp[tax[5], mss[5]] == 0.0
