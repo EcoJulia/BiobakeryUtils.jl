96ba935a48132c031b454b4eb9952c8e1d755e70
Annelle Abatoni Kayisire <aa1@wellesley.edu>
2021-07-21 19:44:37 +0200

    runtests.jl corrections

--- src/communityprofile.jl
+++ src/communityprofile.jl
@@ -1,7 +1,7 @@
-function add_metadata!(comm, file, colname) #how to get the names of the columns in a csv file?
+function add_metadata!(comm, file, colname) #how to get the names of the columns in a csv file? keys(row)
     for row in CSV.File(file)
         for ms in samples(comm)    
-            if row[1] == colname #?
+            if row[1] == colname # check for sample match
                 for i in 1:length(row)
                     set!(ms, :colname, row[i])
                 end
