4dd8b67e6480e29947f14ed59f628187d4a58aba
Kevin Bonham, PhD <kevbonham@gmail.com>
2021-10-08 10:03:41 -0400

    isolation 4

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,11 +15,11 @@ ENV["PATH"] = ENV["PATH"] * ":" * Conda.bin_dir(:BiobakeryUtils)
 end
 
 @testset "Metaphlan" begin
-    profile_1 = metaphlan_profile("files/metaphlan_single1.tsv")
+    # profile_1 = metaphlan_profile("files/metaphlan_single1.tsv")
     # @test profile_1["Bacteria", "metaphlan_single1"] == 100.0
     # @test profile_1["Coriobacteriia", "metaphlan_single1"] == 0.24757
     # @test size(profile_1) == (96, 1)
-    # profile_2 = metaphlan_profile("files/metaphlan_single1.tsv", 3)
+    profile_2 = metaphlan_profile("files/metaphlan_single1.tsv", 3)
     # @test size(profile_2) == (9, 1)
     # @test profile_2["Actinobacteria", "metaphlan_single1"] == 10.84221
     # profile_3 = metaphlan_profile("files/metaphlan_single1.tsv", :phylum)
