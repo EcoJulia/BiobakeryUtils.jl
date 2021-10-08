2467573aec92186fb27b4bdd8e7fa23c5905523d
Kevin Bonham, PhD <kevbonham@gmail.com>
2021-10-08 10:27:55 -0400

    isolation 6

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,34 +20,34 @@ end
     # @test profile_1["Coriobacteriia", "metaphlan_single1"] == 0.24757
     # @test size(profile_1) == (96, 1)
     profile_2 = metaphlan_profile("files/metaphlan_single1.tsv", 3)
-    # @test size(profile_2) == (9, 1)
-    # @test profile_2["Actinobacteria", "metaphlan_single1"] == 10.84221
+    @test size(profile_2) == (9, 1)
+    @test profile_2["Actinobacteria", "metaphlan_single1"] == 10.84221
     profile_3 = metaphlan_profile("files/metaphlan_single1.tsv", :phylum)
-    # @test size(profile_3) == (4, 1)
-    # @test profile_3["Bacteroidetes", "metaphlan_single1"] == 25.60381
+    @test size(profile_3) == (4, 1)
+    @test profile_3["Bacteroidetes", "metaphlan_single1"] == 25.60381
     
-    # merge_profile_1 = metaphlan_profiles("files/metaphlan_multi_test.tsv")
-    # @test size(merge_profile_1) == (42, 7)
-    # @test merge_profile_1["Actinomycetales", "sample3_taxonomic"] == 0.08487
-    # merge_profile_2 = metaphlan_profiles("files/metaphlan_multi_test.tsv", :family)
-    # @test size(merge_profile_2) == (2, 7)
-    # @test merge_profile_2["Actinomycetaceae", "sample7_taxonomic"] == 0.03716
-    # merge_profile_3 = metaphlan_profiles("files/metaphlan_multi_test.tsv", 7)
-    # @test size(merge_profile_3) == (15, 7)
-    # @test merge_profile_3["Actinomyces_viscosus", "sample2_taxonomic"] == 0.03457
+    merge_profile_1 = metaphlan_profiles("files/metaphlan_multi_test.tsv")
+    @test size(merge_profile_1) == (42, 7)
+    @test merge_profile_1["Actinomycetales", "sample3_taxonomic"] == 0.08487
+    merge_profile_2 = metaphlan_profiles("files/metaphlan_multi_test.tsv", :family)
+    @test size(merge_profile_2) == (2, 7)
+    @test merge_profile_2["Actinomycetaceae", "sample7_taxonomic"] == 0.03716
+    merge_profile_3 = metaphlan_profiles("files/metaphlan_multi_test.tsv", 7)
+    @test size(merge_profile_3) == (15, 7)
+    @test merge_profile_3["Actinomyces_viscosus", "sample2_taxonomic"] == 0.03457
 
-    # multi_profile_1 = metaphlan_profiles(["files/metaphlan_single1.tsv", "files/metaphlan_single2.tsv"])
-    # @test size(multi_profile_1) == (129, 2)
-    # @test multi_profile_1["Firmicutes", "metaphlan_single1"] == 63.1582	
-    # @test multi_profile_1["Firmicutes", "metaphlan_single2"] == 48.57123
-    # multi_profile_2 = metaphlan_profiles(["files/metaphlan_single1.tsv", "files/metaphlan_single2.tsv"], 3)
-    # @test size(multi_profile_2) == (11, 2)
-    # @test multi_profile_2["Bacteroidia", "metaphlan_single1"] == 25.60381	
-    # @test multi_profile_2["Bacteroidia", "metaphlan_single2"] == 47.67359	
-    # multi_profile_3 = metaphlan_profiles(["files/metaphlan_single1.tsv", "files/metaphlan_single2.tsv"], :order)
-    # @test size(multi_profile_3) == (13, 2)
-    # @test multi_profile_3["Bifidobacteriales", "metaphlan_single1"] == 10.84221
-    # @test multi_profile_3["Bifidobacteriales", "metaphlan_single2"] == 1.46697	
+    multi_profile_1 = metaphlan_profiles(["files/metaphlan_single1.tsv", "files/metaphlan_single2.tsv"])
+    @test size(multi_profile_1) == (129, 2)
+    @test multi_profile_1["Firmicutes", "metaphlan_single1"] == 63.1582	
+    @test multi_profile_1["Firmicutes", "metaphlan_single2"] == 48.57123
+    multi_profile_2 = metaphlan_profiles(["files/metaphlan_single1.tsv", "files/metaphlan_single2.tsv"], 3)
+    @test size(multi_profile_2) == (11, 2)
+    @test multi_profile_2["Bacteroidia", "metaphlan_single1"] == 25.60381	
+    @test multi_profile_2["Bacteroidia", "metaphlan_single2"] == 47.67359	
+    multi_profile_3 = metaphlan_profiles(["files/metaphlan_single1.tsv", "files/metaphlan_single2.tsv"], :order)
+    @test size(multi_profile_3) == (13, 2)
+    @test multi_profile_3["Bifidobacteriales", "metaphlan_single1"] == 10.84221
+    @test multi_profile_3["Bifidobacteriales", "metaphlan_single2"] == 1.46697	
 
 
     taxstring = "k__Archaea|p__Euryarchaeota|c__Methanobacteria|o__Methanobacteriales|f__Methanobacteriaceae|g__Methanobrevibacter|s__Methanobrevibacter_smithii"
