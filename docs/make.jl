84e2f10856f34a935aed5b9159fc5191944f980d
Kevin Bonham <dev@bonham.ch>
2026-07-16 09:52:40 -0400

    Update documenter to v1

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,7 @@ using Documenter, BiobakeryUtils
 
 makedocs(
     sitename = "BiobakeryUtils.jl",
+    warnonly = [:missing_docs],
     pages = [
         "BiobakeryUtils" => "index.md",
         "Getting Started" => "gettingstarted.md",
@@ -9,13 +10,14 @@ makedocs(
         "Working with MetaPhlAn" => "metaphlan.md",
         "Working with HUMAnN" => "humann.md",
         "Microbiome.jl Docstrings" => "microbiome.md",
-        ],
+    ],
     authors = "Kevin Bonham, PhD",
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true",
-        edit_link="main",
-        canonical="http://docs.ecojulia.org/BiobakeryUtils.jl/stable/"),
-        )
+        edit_link = "main",
+        canonical = "https://docs.ecojulia.org/BiobakeryUtils.jl/stable/",
+    ),
+)
 
 deploydocs(
     repo = "github.com/EcoJulia/BiobakeryUtils.jl.git",
